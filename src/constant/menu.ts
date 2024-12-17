efe0a20e21a67e57b01e2b3c2d4d6da139fbc279
zhekabaila <zhekabaila@gmail.com>
2024-12-17 22:21:52 +0700

    refactor: new version

---
+++ src/constant/menu.ts
@@ -0,0 +1,8 @@
+export const menus = [
+  { label: 'Home', href: '/' },
+  { label: 'About', href: '/about' },
+  { label: 'Portfolio', href: '/portfolio' },
+  { label: 'Resume', href: '/resume' },
+  { label: 'Experience', href: '/experience' },
+  { label: 'Contact', href: '/contact' },
+]
